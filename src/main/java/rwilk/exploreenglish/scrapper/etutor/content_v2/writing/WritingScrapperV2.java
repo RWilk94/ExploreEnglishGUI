d5aa0d2c0af2cb7f7b3b8e0d8350ebc91afc1d66
Rafal Wilk <rwilk@corelogic.com>
2024-12-03 18:08:15 +0100

    Fix WritingScrapperV2

--- src/main/java/rwilk/exploreenglish/scrapper/etutor/content_v2/writing/WritingScrapperV2.java
+++ src/main/java/rwilk/exploreenglish/scrapper/etutor/content_v2/writing/WritingScrapperV2.java
@@ -1,5 +1,9 @@
 package rwilk.exploreenglish.scrapper.etutor.content_v2.writing;
 
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,19 +15,20 @@ import rwilk.exploreenglish.model.entity.etutor.EtutorExercise;
 import rwilk.exploreenglish.model.entity.etutor.EtutorNote;
 import rwilk.exploreenglish.repository.etutor.EtutorExerciseRepository;
 import rwilk.exploreenglish.repository.etutor.EtutorNoteRepository;
-import rwilk.exploreenglish.scrapper.etutor.BaseScrapper;
+import rwilk.exploreenglish.scrapper.etutor.content_v2.BaseNoteScrapperV2;
 import rwilk.exploreenglish.scrapper.etutor.type.ExerciseType;
 
 import java.util.ArrayList;
 
 @Component
-public class WritingScrapperV2 extends BaseScrapper implements CommandLineRunner {
+public class WritingScrapperV2 extends BaseNoteScrapperV2 implements CommandLineRunner {
 
     private final EtutorExerciseRepository etutorExerciseRepository;
     private final EtutorNoteRepository etutorNoteRepository;
 
     public WritingScrapperV2(EtutorExerciseRepository etutorExerciseRepository,
                              EtutorNoteRepository etutorNoteRepository) {
+        super(etutorExerciseRepository, etutorNoteRepository);
         this.etutorExerciseRepository = etutorExerciseRepository;
         this.etutorNoteRepository = etutorNoteRepository;
     }
@@ -31,7 +36,10 @@ public class WritingScrapperV2 extends BaseScrapper implements CommandLineRunner
     @Override
     public void run(final String... args) throws Exception {
 //        etutorExerciseRepository.findAllByTypeAndIsReady(ExerciseType.WRITING.toString(), false)
-//                .forEach(this::webScrap);
+//                .forEach(it -> {
+//                    final WebDriver driver = super.getDriver();
+//                    webScrap(it, driver);
+//                });
     }
 
     public void webScrap(final EtutorExercise etutorExercise, final WebDriver driver) {
@@ -47,26 +55,46 @@ public class WritingScrapperV2 extends BaseScrapper implements CommandLineRunner
         // close cookie box
         super.closeCookieBox(driver);
 
-        WebElement nativeLessonContent;
-        if (!driver.findElements(By.className("nativeLessonContent")).isEmpty()) {
-            nativeLessonContent = driver.findElement(By.className("nativeLessonContent"));
-        } else {
-            nativeLessonContent = driver.findElement(By.className("exercise"));
-        }
-
         final EtutorNote etutorNote = EtutorNote.builder()
-                .nativeContent(nativeLessonContent.findElement(By.className("contentBox")).getText())
-                .nativeHtml(nativeLessonContent.getAttribute("innerHTML"))
                 .noteItems(new ArrayList<>())
                 .exercise(etutorExercise)
                 .build();
+
+        if (!driver.findElements(By.className("nativeLessonContent")).isEmpty()) {
+            final WebElement nativeLessonContent = driver.findElement(By.className("nativeLessonContent"));
+            etutorNote.setNativeContent(nativeLessonContent.findElement(By.className("contentBox")).getText());
+            etutorNote.setNativeHtml(nativeLessonContent.getAttribute("innerHTML"));
+        }
+
         if (!driver.findElements(By.className("foreignTranslationButton")).isEmpty()) {
             final WebElement foreignLessonContent = driver.findElement(By.className("foreignLessonContent"));
             driver.findElement(By.className("foreignTranslationButton")).click();
+
             etutorNote.setForeignContent(foreignLessonContent.findElement(By.className("contentBox")).getText());
             etutorNote.setForeignHtml(foreignLessonContent.getAttribute("innerHTML"));
 
             driver.findElement(By.className("nativeTranslationButton")).click();
+
+        } else if (!driver.findElements(By.className("foreignLessonContent")).isEmpty()) {
+            final WebElement foreignLessonContent = driver.findElement(By.className("foreignLessonContent"));
+            etutorNote.setForeignContent(foreignLessonContent.findElement(By.className("contentBox")).getText());
+            etutorNote.setForeignHtml(foreignLessonContent.getAttribute("innerHTML"));
+        }
+
+        if (StringUtils.isNoneBlank(etutorNote.getNativeContent())) {
+            final Document jsoupHtml = Jsoup.parse(etutorNote.getNativeHtml());
+            final Element rootElement = jsoupHtml.getAllElements().first();
+            if (rootElement != null) {
+                super.printLeafNodesRecursive(rootElement, etutorNote, "native");
+            }
+        }
+
+        if (StringUtils.isNoneBlank(etutorNote.getForeignHtml())) {
+            final Document jsoupForeignHtml = Jsoup.parse(etutorNote.getForeignHtml());
+            final Element rootForeignElement = jsoupForeignHtml.getAllElements().first();
+            if (rootForeignElement != null) {
+                super.printLeafNodesRecursive(rootForeignElement, etutorNote, "foreign");
+            }
         }
 
         etutorNoteRepository.save(etutorNote);
